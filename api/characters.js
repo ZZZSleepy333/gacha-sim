077f194145a6fa0bec440f60e22466d1de9411b7
KallaneKoe <namnguyenhoang0903@gmail.com>
2025-01-31 20:28:31 +0700

    commit

--- api/characters.js
+++ api/characters.js
@@ -7,42 +7,42 @@ const DB_NAME = "banner_db";
 
 let db;
 
-const crawlCharacters = async () => {
-  try {
-    const url = "https://housamo-skill.netlify.app/charas/"; // Thay đổi URL nếu cần
-    const { data } = await axios.get(url);
-    const $ = cheerio.load(data);
-    const characters = [];
+// const crawlCharacters = async () => {
+//   try {
+//     const url = "https://housamo-skill.netlify.app/charas/"; // Thay đổi URL nếu cần
+//     const { data } = await axios.get(url);
+//     const $ = cheerio.load(data);
+//     const characters = [];
 
-    $("#chara-list li").each((_, element) => {
-      const $el = $(element);
-      const name = $el.find("a").text().trim();
-      const href = $el.find("a").attr("href");
-      const rarity = $el.attr("data-rarity");
-      const imgSrc = $el.find("img").attr("src");
+//     $("#chara-list li").each((_, element) => {
+//       const $el = $(element);
+//       const name = $el.find("a").text().trim();
+//       const href = $el.find("a").attr("href");
+//       const rarity = $el.attr("data-rarity");
+//       const imgSrc = $el.find("img").attr("src");
 
-      // Xử lý tags
-      let tags = [];
-      if (name.includes("Standard")) tags.push("Normal");
-      else tags.push("Limited");
+//       // Xử lý tags
+//       let tags = [];
+//       if (name.includes("Standard")) tags.push("Normal");
+//       else tags.push("Limited");
 
-      if (!name.includes("(")) tags.push("New");
+//       if (!name.includes("(")) tags.push("New");
 
-      characters.push({
-        name,
-        href,
-        rarity: parseInt(rarity, 10),
-        imgSrc,
-        tags,
-      });
-    });
+//       characters.push({
+//         name,
+//         href,
+//         rarity: parseInt(rarity, 10),
+//         imgSrc,
+//         tags,
+//       });
+//     });
 
-    return characters;
-  } catch (error) {
-    console.error("❌ Lỗi khi crawl dữ liệu:", error);
-    return [];
-  }
-};
+//     return characters;
+//   } catch (error) {
+//     console.error("❌ Lỗi khi crawl dữ liệu:", error);
+//     return [];
+//   }
+// };
 
 const connectDB = async () => {
   if (!db) {
